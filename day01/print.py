75bd8f966b98bf96fb659937ca14113b76e8c92c
Mayank Madhav <Mayank.Madhav@Calabrio.com>
2024-02-13 12:37:34 +0530

    Day 01 wrap up commit

---
+++ day01/print.py
@@ -0,0 +1,5 @@
+# Day 01 - Python Print challenge
+print("Hello, World!")
+print("I am on 100 days of Python challenge ...")
+
+print("Hello" + " " + "Mayank")
---
+++ day01/swap.py
@@ -0,0 +1,14 @@
+a = input("Enter number 1: ")
+b = input("Enter number 2: ")
+
+print("Number 1: " + a)
+print("Number 2: " + b)
+
+temp = a
+a = b
+b = temp
+
+print("Swapping ...")
+
+print("Number 1: " + a)
+print("Number 2: " + b)
